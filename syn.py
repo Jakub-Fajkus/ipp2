488b2be21b95caed85e1da1f84a8e4830c5cbea7
Jakub Fajkus <jakub.fajkus@gmail.com>
2017-03-29 19:37:36 +0200

    basic exception handling

--- syn.py
+++ syn.py
@@ -1,12 +1,14 @@
+import sys
 from app.arguments import ArgumentsParser, Config
-
+from app.arguments.exceptions.InvalidArgumentsException import InvalidArgumentsException
 
 config = Config.Config()
 argParser = ArgumentsParser.ArgumentsParser()
-argParser.parseArguments(config=config)
 
+try:
+    argParser.parseArguments(config=config)
+except InvalidArgumentsException as error:
+    with open(sys.stderr, 'r') as f:
+        f.write(error.message)
 
-
-
-
-
+    sys.exit(error.code)
